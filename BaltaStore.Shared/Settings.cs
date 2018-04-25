27e7ccdbf9d37ca89ddfdcff2861404383395adb
alexcanario <alexcanario@gmail.com>
2018-04-25 15:55:26 -0300

    20180405 Comentario da propriedade ConnectionString

--- BaltaStore.Shared/Settings.cs
+++ BaltaStore.Shared/Settings.cs
@@ -1,5 +1,6 @@
 namespace BaltaStore.Shared {
     public static class Settings {
+        //Retorna a string de conexão
         public static string ConnectionString => @"Server=.\MSSQLSERVER; Database=BaltaStore; User ID=sa; Password=Ca220899";
     }
 }
